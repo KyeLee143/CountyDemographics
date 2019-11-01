4b082daff92a4917d9811b938053e65b428be223
KyeLee143 <54326810+KyeLee143@users.noreply.github.com>
2019-11-01 09:48:11 -0700

    Update Demographics.py

--- Demographics.py
+++ Demographics.py
@@ -1,3 +1,10 @@
 import flask import Flask, request, Markup, render_template, flash, Markup
 import os 
 import json 
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+
+
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
