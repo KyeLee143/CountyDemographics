dcd107f8a3c4a3224328922c5184eb4310988144
KyeLee143 <54326810+KyeLee143@users.noreply.github.com>
2019-11-01 10:00:17 -0700

    Update Demographics.py
    WIP. Need to add listOfStates code

--- Demographics.py
+++ Demographics.py
@@ -4,6 +4,16 @@ import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+def your_interesting_demographic_function(counties):
+    get_state_optons
+        List lostOfStates is initialized to an empty list
+        for each county in counties
+            if data ['county'] not in listOfStates
+                add 
 
 
 if __name__=="__main__":
